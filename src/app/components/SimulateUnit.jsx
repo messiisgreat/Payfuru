f429aab1c77981b8c32ae4b5aef09c586e7d66b7
messiisgreat <superonecoderman@gmail.com>
2024-05-28 09:05:49 +0900

    payfirst commit

---
+++ src/app/components/SimulateUnit.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
+
+const SimulateUnit = ({
+  unitKey,
+  item,
+  simulateArray,
+  setSimulateArray,
+  donateCal,
+}) => {
+  const [productPrice, setProductPrice] = useState(item.price);
+  const [productAmount, setProductAmount] = useState(item.count);
+  const simulateRemove = () => {
+    let tmp = simulateArray;
+    const newIndex = tmp.findIndex((element) => element.id === unitKey);
+    tmp.splice(newIndex, 1);
+    setSimulateArray([...tmp]);
+  };
+
+  useEffect(() => {
+    let tmp = simulateArray;
+    tmp.forEach((element) => {
+      if (element.id === unitKey) {
+        element.price = productPrice;
+        element.count = productAmount;
+      }
+    });
+    donateCal();
+    setSimulateArray(tmp);
+  }, [productPrice, productAmount]);
+
+  return (
+    <div className="flex m-auto my-4 items-center justify-center gap-3">
+      {simulateArray.length === 1 ? null : (
+        <Icon icon="ei:minus" width="32" height="32" onClick={simulateRemove} />
+      )}
+      <div className="flex">
+        <input
+          type="number"
+          className="border-grayLetter border px-3 py-2.5"
+          value={productPrice == 0 ? "" : productPrice}
+          onChange={(e) => setProductPrice(Number(e.target.value))}
+        />
+        <div className="items-center flex">
+          <p className="mx-2">円</p>
+        </div>
+      </div>
+      <div className="flex">
+        <select
+          type="number"
+          className="border-grayLetter border px-3 py-3"
+          value={productAmount}
+          onChange={(e) => setProductAmount(Number(e.target.value))}
+        >
+          {Array.from({ length: 100 }, (_, i) => (
+            <option key={i + 1} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+        <div className="items-center flex">
+          <p className="mx-2">個</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SimulateUnit;
